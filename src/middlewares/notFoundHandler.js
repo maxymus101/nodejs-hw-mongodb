e2683769f750c0a7bb0368393dba81d98e8bb58c
Max Akimov <maxymus2828@gmail.com>
2025-08-08 22:29:03 +0300

    fix 1.0 of notFoundHandler

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,18 +1,5 @@
-import { HttpError } from 'http-errors';
+import createHttpError from 'http-errors';
 
-export const notFoundHandler = (err, req, res, next) => {
-  if (err instanceof HttpError) {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.name,
-      data: err,
-    });
-    return;
-  }
-
-  res.status(404).json({
-    status: 404,
-    message: 'Route not found',
-    data: err.message,
-  });
+export const notFoundHandler = (req, res, next) => {
+  next(createHttpError(404, 'Route not found'));
 };
