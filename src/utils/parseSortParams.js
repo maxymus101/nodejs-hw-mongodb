e6af0cb542dfbef254eb88f98e315f452b240969
Max Akimov <maxymus2828@gmail.com>
2025-08-16 17:54:47 +0200

    added utils to parse params

---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,34 @@
+import { SORT_ORDER } from '../constatns/sortOrder.js';
+
+const parseSortOrder = (sortOrder) => {
+  const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
+  if (isKnownOrder) return sortOrder;
+  return SORT_ORDER.ASC;
+};
+
+const parseSortBy = (sortBy) => {
+  const keysOfContact = [
+    '_id',
+    'name',
+    'phoneNumber',
+    'email',
+    'isFavourite',
+    'contactType',
+  ];
+  if (keysOfContact.includes(sortBy)) {
+    return sortBy;
+  }
+  return '_id';
+};
+
+export const parseSortParams = (query) => {
+  const { sortBy, sortOrder } = query;
+
+  const parsedSortOrder = parseSortOrder(sortOrder);
+  const parsedSortBy = parseSortBy(sortBy);
+
+  return {
+    sortOrder: parsedSortOrder,
+    sortBy: parsedSortBy,
+  };
+};
