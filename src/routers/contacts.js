6d648ce18ae054bc4f64482190fc35151671053e
Max Akimov <maxymus2828@gmail.com>
2025-08-16 17:56:49 +0200

    update routes

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,16 +7,33 @@ import {
   deleteContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactValidationSchema } from '../validation/createContactValidationSchema.js';
+import { patchContactValidationSchema } from '../validation/patchContactValidationSchema.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
 
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post(
+  '/contacts',
+  validateBody(createContactValidationSchema),
+  ctrlWrapper(createContactController),
+);
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(patchContactValidationSchema),
+  ctrlWrapper(patchContactController),
+);
 
 router.delete('/contact/:contactId', ctrlWrapper(deleteContactController));
 
