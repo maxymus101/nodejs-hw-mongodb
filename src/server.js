3fe7d4164b51a89f5bbd9b571d608ec383463fe4
Max Akimov <maxymus2828@gmail.com>
2025-09-01 15:22:23 +0200

    added usage of  cookieParser

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import { getEnvVar } from './utils/getEnvVar.js';
 import router from './routers/index.js';
@@ -13,6 +14,7 @@ export const setupServer = () => {
   const PORT = Number(getEnvVar('PORT', '3000'));
 
   app.use(cors());
+  app.use(cookieParser());
   app.use(
     pino({
       transport: {
