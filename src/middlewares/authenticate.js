862c0606f5a0cbcf0a5be3d285bdb25dbbb11691
Max Akimov <maxymus2828@gmail.com>
2025-09-01 18:04:34 +0200

    fix import path

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,6 +1,6 @@
 import createHttpError from 'http-errors';
-import { SessionCollection } from '../db/models/session';
-import { UsersCollection } from '../db/models/user';
+import { SessionCollection } from '../db/models/session.js';
+import { UsersCollection } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
