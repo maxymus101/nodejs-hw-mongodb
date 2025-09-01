6cc4f4e4b87731191ab40034ee7042ff84c8cfc2
Max Akimov <maxymus2828@gmail.com>
2025-09-01 02:21:37 +0200

    work on user registration

---
+++ src/routers/auth.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { registerUserSchema } from '../validation/auth.js';
+import { registerUserController } from '../controllers/auth.js';
+import { validateBody } from '../middlewares/validateBody.js';
+
+const router = Router();
+
+router.post(
+  '/register',
+  validateBody(registerUserSchema),
+  ctrlWrapper(registerUserController),
+);
+
+router.post('/login', validateBody(), ctrlWrapper());
+
+export default router;
