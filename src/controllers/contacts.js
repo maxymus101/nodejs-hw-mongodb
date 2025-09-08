f25bfe50bbf7aaa15ade7e2e1e3b5519a31c0b11
Max Akimov <maxymus2828@gmail.com>
2025-09-08 15:39:44 +0200

    fixed logic of saving photos

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -9,7 +9,6 @@ import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
-import { getEnvVar } from '../utils/getEnvVar.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
 import { ENABLE_CLOUDINARY } from '../constants/index.js';
@@ -85,7 +84,7 @@ export const patchContactController = async (req, res, next) => {
   let photoUrl;
 
   if (photo) {
-    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+    if (ENABLE_CLOUDINARY === 'true') {
       photoUrl = await saveFileToCloudinary(photo);
     } else {
       photoUrl = await saveFileToUploadDir(photo);
